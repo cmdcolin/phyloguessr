a964596b9ea76186fa30cd7ea842142b4512e4a5
Colin <colin.diesh@gmail.com>
2026-03-06 11:00:23 -0500

    Updates

--- _astro/history.CYt9l2jZ.js
+++ _astro/history.CYt9l2jZ.js
@@ -1,1 +1,2 @@
 function n(){const e=localStorage.getItem("phyloHistory");return e?JSON.parse(e):[]}function o(e){const t=e.length>500?e.slice(e.length-500):e;return localStorage.setItem("phyloHistory",JSON.stringify(t)),t}function a(e){let t=0;for(let r=e.length-1;r>=0&&e[r].correct;r--)t++;return t}export{a as g,n as l,o as s};
+//# sourceMappingURL=history.CYt9l2jZ.js.map
