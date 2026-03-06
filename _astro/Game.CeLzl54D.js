903ea3076d0fba9f4de9068c856029f0d81712b9
Colin <colin.diesh@gmail.com>
2026-03-06 14:59:21 -0500

    Updates

--- _astro/Game.CeLzl54D.js
+++
@@ -1,2 +0,0 @@
-import{G as d}from"./Game.BNyyyVw8.js";import"./jsxRuntime.module.B_E2gpJn.js";import"./preact.module.CK4bCYyt.js";import"./hooks.module.Ct3O3C33.js";import"./Header.BDuARoTB.js";import"./firebase.C6epVS_3.js";import"./preload-helper.BBLiYfsK.js";import"./history.CYt9l2jZ.js";export{d as default};
-//# sourceMappingURL=Game.CeLzl54D.js.map
---
+++ _astro/storage.WvAOMQpp.js
@@ -0,0 +1,2 @@
+function t(e){return typeof localStorage<"u"?localStorage.getItem(e):null}function n(e){return typeof sessionStorage<"u"?sessionStorage.getItem(e):null}export{t as l,n as s};
+//# sourceMappingURL=storage.WvAOMQpp.js.map
