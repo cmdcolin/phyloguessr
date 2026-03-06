c0a60252de06fd4914be4a9195fb6e78528c2b49
Colin <colin.diesh@gmail.com>
2026-03-06 11:00:12 -0500

    Prod source maps

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,9 @@ export default defineConfig({
   site: "https://cmdcolin.github.io",
   base: "/phyloguessr/",
   integrations: [preact({ compat: true })],
+  vite: {
+    build: {
+      sourcemap: true,
+    },
+  },
 });
